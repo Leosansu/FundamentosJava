b41eeb7bd2dd13dc5765c215eb42829f08a29b7a
Leonam <leosuerte_@hotmail.com>
2023-12-02 16:52:25 -0300

    Classe Aluguel Quarto

---
+++ AluguelQuarto.java
@@ -0,0 +1,41 @@
+public class AluguelQuarto {
+   private String nome;
+   private Integer quarto;
+
+   public AluguelQuarto(){
+
+        }
+
+   public AluguelQuarto(String nome, Integer quarto){
+       this.nome = nome;
+       this.quarto = quarto;
+
+
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getQuarto() {
+            return quarto;
+        }
+
+        public void setQuarto(Integer quarto){
+            this.quarto = quarto;
+        }
+
+
+
+        @Override
+        public String toString() {
+            return "Cliente: "+ nome
+                    +" , Quarto:"+quarto;
+
+        }
+    }
+
